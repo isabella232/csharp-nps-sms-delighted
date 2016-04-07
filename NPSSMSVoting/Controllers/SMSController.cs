afa1309939d4b5c38c08cd5975efe0a1639819a2
spacedsweden <christian.jensen@spaced.se>
2016-04-07 15:09:01 -0700

    small changes

--- NPSSMSVoting/Controllers/SMSController.cs
+++ NPSSMSVoting/Controllers/SMSController.cs
@@ -8,35 +8,29 @@ using System.Web.Http;
 using Delighted.Api.DelightedData;
 using SMS_NPS_Delighted.Models;
 
-namespace NPSSMSVoting.Controllers
-{
-    public class SMSController : ApiController
-    {
+namespace NPSSMSVoting.Controllers {
+    public class SMSController : ApiController {
         [Route("SMS")]
         [HttpPost]
         public async Task<HttpResponseMessage> Index(SMSCallbackModel model) {
-            var dapi = new Delighted.Api.Client("yourkey");
-            var person = await dapi.AddPerson(new Person
-            {
+            var client = new Delighted.Api.Client("yourkey");
+            var person = await client.AddPerson(new Person {
                 Email = model.From.Endpoint + "@sinch.com",
                 Send = false
             });
-            if (person != null)
-            {
+            if (person != null) {
                 int score;
                 if (model.Message.Substring(0, 2) == "10") {
                     score = 10;
-                }
-                else {
+                } else {
                     int.TryParse(model.Message.Substring(0, 1), out score);
                 }
-                string comment = model.Message.Length > 1 ? model.Message.Substring(score==10 ? 2:1) : "";
+                string comment = model.Message.Length > 1 ? model.Message.Substring(score == 10 ? 2 : 1) : "";
                 if (model.Message != "0" && score == 0)
                     return new HttpResponseMessage(HttpStatusCode.OK);
                 var dic = new Dictionary<string, string>();
                 dic.Add("survey_origin", "APIWorld");
-                await dapi.AddResult(new AddResult()
-                {
+                await client.AddResult(new AddResult() {
                     Score = score,
                     Comment = comment,
                     PersonId = person.Id.ToString(),
